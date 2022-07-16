781983717a79029aada26c8dc0a0dfe0f4c5ea79
JessicaRudd <jess@irudd.com>
2022-07-16 16:06:11 -0400

    updates to README

--- {{ cookiecutter.repo_name }}/setup.py
+++ {{ cookiecutter.repo_name }}/setup.py
@@ -17,7 +17,7 @@ dev_packages = [
     "black==20.8b1",
     "flake8==3.8.3",
     "isort==5.9.3",
-    "mccabe==0.7.0",
+    "mccabe==0.6.0",
     "pre-commit==2.19.0",
     "jupyterlab>=3.0.0",
     "jupyterlab-templates==0.3.1",
